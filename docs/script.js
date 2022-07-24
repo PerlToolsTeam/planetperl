4aa716918938dfb3e38697d2d8e0aea7dcff748f
Dave Cross <dave@dave.org.uk>
2022-07-24 16:00:55 +0100

    Store show/hide in a cookie

--- docs/script.js
+++ docs/script.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
     switch (e.which) {
       case 104:
         $('.card-body').hide();
+        setCookie('showhide', 'hide', 1000);
         break;
       case 110:
         next_div();
@@ -21,6 +22,7 @@ $(document).ready(function() {
         break;
       case 115:
         $('.card-body').show();
+        setCookie('showhide', 'show', 1000);
         break;
     }
   });
@@ -31,6 +33,12 @@ $(document).ready(function() {
   $('.arrow-down').click(function() {
     move_to($(this).parent().parent().parent().next);
   });
+
+
+  var showhide = getCookie('showhide');
+  if (showhide == 'hide') {
+    $('.card-body').hide();
+  }
 });
 
 function get_curr_card() {
@@ -61,3 +69,27 @@ function prev_div() {
   var card = get_curr_card().prev();
   move_to(card);
 }
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
